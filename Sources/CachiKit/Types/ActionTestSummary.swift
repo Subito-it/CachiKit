ee30e77660b1ed579525c0c4346f416e5a929fb6
Tomas Camin <tomas.camin@adevinta.com>
2020-09-29 11:16:26 +0200

    Handle missing duration in 'System Failures'

--- Sources/CachiKit/Types/ActionTestSummary.swift
+++ Sources/CachiKit/Types/ActionTestSummary.swift
@@ -2,7 +2,7 @@ import Foundation
 
 public class ActionTestSummary: ActionTestSummaryIdentifiableObject {
     public let testStatus: String
-    public let duration: Double
+    public let duration: Double?
     public let performanceMetrics: [ActionTestPerformanceMetricSummary]
     public let failureSummaries: [ActionTestFailureSummary]
     public let skipNoticeSummary: ActionTestNoticeSummary?
@@ -21,7 +21,7 @@ public class ActionTestSummary: ActionTestSummaryIdentifiableObject {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         
         self.testStatus = try container.decodeValue(String.self, forKey: .testStatus)
-        self.duration = try  container.decodeValue(Double.self, forKey: .duration)
+        self.duration = try  container.decodeValueIfPresent(Double.self, forKey: .duration) ?? 0
         self.performanceMetrics = try container.decodeValuesIfPresent(ActionTestPerformanceMetricSummary.self, forKey: .performanceMetrics) ?? []
         self.failureSummaries = try container.decodeValuesIfPresent(ActionTestFailureSummary.self, forKey: .failureSummaries) ?? []
         self.skipNoticeSummary =  try container.decodeValueIfPresent(ActionTestNoticeSummary.self, forKey: .skipNoticeSummary)
