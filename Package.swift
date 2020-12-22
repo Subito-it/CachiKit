bfb930517df1c53b284000efa7c7e80bb5772b63
Tomas Camin <tomas.camin@adevinta.com>
2020-12-22 22:46:53 +0100

    Update dependency

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/JohnSundell/ShellOut.git", .branch("master")),
+        .package(url: "https://github.com/tcamin/ShellOut.git", .branch("master")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
